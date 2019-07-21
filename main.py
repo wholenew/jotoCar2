0f9644dc133b1cd8edb558049a537b6f50d3e128
Your Name <you@example.com>
2019-07-21 17:11:36 +0900

    追尾実装

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import cv2
 from flask import Flask, Response
 
 from models.car_manager import CarManager
+
+import picamera
+import picamera.array
 
 app = Flask(__name__)
 
@@ -24,6 +29,7 @@ print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print("fps: " + str(cap.get(cv2.CAP_PROP_FPS)))
 
 face_cascade_path = './haarcascade_frontalface_default.xml'
+#face_cascade_path = './haarcascade_eye_tree_eyeglasses.xml'
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 def getCarManager():
@@ -32,33 +38,39 @@ def getCarManager():
 def getFrames():
     while True:
         ret, frame = cap.read()
+        #print(frame)
         # frame = cv2.flip(frame, 1) # horizontal flip
         # ret, jpg = cv2.imencode("test.jpg", frame)
         src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(src_gray, 1.3, 5)
-
-        for x, y, w, h in faces:
-            # print(x,y,w,h)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
-            # face = jpg[y: y + h, x: x + w]
-            # face_gray = src_gray[y: y + h, x: x + w]
-            faceCenterX = x + (w/2)
-            faceCenterY = y + (h/2)
-            faceArea = w * h
-            percentFace = faceArea / frame_area
-            diffX = frame_centerX - faceCenterX
-            diffY = frame_centerY - faceCenterY
-            carManager = getCarManager()
-            if diffX < -30:
-                carManager.left()
-            elif diffX < 30:
-                carManager.right()
-            elif percentFace > 0.3:
-                carManager.back()
-            elif percentFace < 0.02:
-                carManager.forward()
-            else:
-                carManager.stop()
+        carManager = getCarManager()
+        if len(faces) > 0:
+            for x, y, w, h in faces:
+                # print(x,y,w,h)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+                # face = jpg[y: y + h, x: x + w]
+                # face_gray = src_gray[y: y + h, x: x + w]
+                faceCenterX = x + (w/2)
+                faceCenterY = y + (h/2)
+                faceArea = w * h
+                percentFace = faceArea / frame_area
+                diffX = frame_centerX - faceCenterX
+                diffY = frame_centerY - faceCenterY
+                #print(percentFace)
+                #print(diffX)
+                if percentFace > 0.1:
+                    carManager.back()
+                elif percentFace < 0.05:
+                    carManager.forward()
+                    
+                elif diffX < -80:
+                    carManager.right()
+                elif diffX > 80:
+                    carManager.left()
+                else:
+                    carManager.stop()
+        else:
+            carManager.stop()
 
         ret, jpg = cv2.imencode("test.jpg", frame)
         yield b'--boundary\r\nContent-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n\r\n'
@@ -69,5 +81,5 @@ def video_feed():
 
 # import webbrowser
 # webbrowser.get().open("192.168.0.101:5001")
-
-app.run(host = '192.168.0.101', port = 5001, threaded = False) # only 1 client
+if __name__ == '__main__':
+    app.run(host = '192.168.0.103', port = 5001, threaded = False) # only 1 client
--- models/car_manager.py
+++ models/car_manager.py
@@ -1,9 +1,9 @@
 import logging
-import socket
+#import socket
 import sys
-import time
+# import time
 import pigpio
-import serial
+# import serial
 # import smbus
 
 from models.base import Singleton
@@ -16,67 +16,99 @@ R_IN2 = 21
 L_PWM = 17
 L_IN1 = 27
 L_IN2 = 22
-frequency = 300
+frequency = 200
 maxPwm = 255
 
 class CarManager(metaclass=Singleton):
   
   def __init__(self, servo=4):
-        self.response = None
+        # self.response = None
         self.pi = pigpio.pi()
-        self.servo = servo
+        # self.servo = servo
         # self.bus = smbus.SMBus(1)
-        self.address = 0x04
+        # self.address = 0x04
 
         self.pi.set_mode(R_IN1, pigpio.OUTPUT)
         self.pi.set_mode(R_IN2, pigpio.OUTPUT)
         self.pi.set_mode(L_IN1, pigpio.OUTPUT)
         self.pi.set_mode(L_IN2, pigpio.OUTPUT)
 
-        self.pi.set_PWM_frequency(R_PWM,  self.frequency)
-        self.pi.set_PWM_range(R_PWM,  self.maxPwm)
-        self.pi.set_PWM_frequency(L_PWM, self.frequency)
-        self.pi.set_PWM_range(L_PWM, self.maxPwm)
-
-
-        self.set_PWM_dutycycle(R_PWM, 125)
-        self.set_PWM_dutycycle(L_PWM, 125)
-        self.pi.write(R_IN1, 1)
-        self.pi.write(R_IN2, 1)
-        self.pi.write(L_IN1, 1)
-        self.pi.write(L_IN2, 1)
-        time.sleep(10);
+        self.pi.set_PWM_frequency(R_PWM, frequency)
+        self.pi.set_PWM_range(R_PWM,maxPwm)
+        self.pi.set_PWM_frequency(L_PWM, frequency)
+        self.pi.set_PWM_range(L_PWM, maxPwm)
 
   def send_command(self, command):
-      logger.info({'action': 'send_command', 'command': command})
-      code = ''
+      # logger.info({'action': 'send_command', 'command': command})
+      code = 's'
+      r_pwm = 255
+      l_pwm = 255
+  
       if command=='forward':
+        r_pwm = 255
+        l_pwm = 255
         code='f'
-        pwm=1450
+     
       elif command=='back':
+        r_pwm = 255
+        l_pwm = 255        
         code='b'
-        pwm =1450
       elif command=='right':
+        r_pwm = 200
+        l_pwm = 255
         code='r'
-        pwm =500
       elif command=='left':
+        r_pwm = 255
+        l_pwm = 200
         code='l'
-        pwm =2350
       elif command=='stop':
+        r_pwm = 0
+        l_pwm = 0
         code='s'
-        pwm =1450
 
-
-      self.pi.set_servo_pulsewidth(self.servo, pwm)
+      if code=='f':
+          r_in1 = 1
+          r_in2 = 0
+          l_in1 = 1
+          l_in2 = 0
+          print('f') 
+      elif code=='b':
+          r_in1 = 0
+          r_in2 = 1
+          l_in1 = 0
+          l_in2 = 1
+          print('b')
+      elif code=='r':
+          r_in1 = 1
+          r_in2 = 0
+          l_in1 = 1
+          l_in2 = 0
+          print('r')
+      elif code=='l':
+          r_in1 = 1
+          r_in2 = 0
+          l_in1 = 1
+          l_in2 = 0
+          print('l')
+      elif code=='s':
+          r_in1 = 1
+          r_in2 = 1
+          l_in1 = 1
+          l_in2 = 1
+          #print('s')
+      self.pi.set_PWM_dutycycle(R_PWM, r_pwm)
+      self.pi.set_PWM_dutycycle(L_PWM, l_pwm)
+      self.pi.write(R_IN1, r_in1)
+      self.pi.write(R_IN2, r_in2)
+      self.pi.write(L_IN1, l_in1)
+      self.pi.write(L_IN2, l_in2)
+      #  self.pi.set_servo_pulsewidth(self.servo, pwm)
       # self.bus.write_byte(self.address, ord(code))
       # time.sleep(3)
 
       # return self.response
 
   def forward(self):
-
-
-
       return self.send_command('forward')
 
   def back(self):
